079db069ab04575e42bd2cea366629424ba9d33d
Magi Feeney <matrixfeeney@gmail.com>
2026-01-15 16:14:52 +0800

    Prioritized(Memory) done!

--- agent/memory.py
+++ agent/memory.py
@@ -86,7 +86,7 @@ class Uniform(Memory):
     def sample_batch_index(self, batch_size: int, key: PRNGKeyArray, *, chunk_size: int):
         start = jax.lax.where(
             self.full,
-            self.ptr - self.capacity, # Prevent overshooting due to wrapping
+            self.ptr - self.capacity, # Prevent overshooting due to wrapping; if negative, turn non-contiguous intervals into a contiguous one, facilitating sampling efficiency
             0
         )
         end = self.ptr - chunk_size + 1
@@ -98,24 +98,38 @@ class Uniform(Memory):
 # Data structure for more efficient sampling w.r.t. priority
 class SumTree(eqx.Module):
     tree: jax.Array
+    masked_priorities: jax.Array                  # Priorities of last chunk (to prevent overshooting)
     active_leaves: int = eqx.field(static=True)   # Requested capacity; no need to be power of 2
     capacity: int = eqx.field(static=True)        # Real capacity of power of 2
     depth: int = eqx.field(static=True)
+    chunk_size: int = eqx.field(static=True)
 
-    def __init__(self, active_leaves: int):
+    def __init__(self, capacity: int, chunk_size: int):
         self.active_leaves = capacity
         self.depth = int(jnp.ceil(jnp.log2(capacity)))
         self.capacity = 2 ** self.depth
-
         self.tree = jnp.zeros(2 * self.capacity - 1)
+
+        self.chunk_size = chunk_size
+        self.masked_priorities = jnp.zeros(chunk_size - 1) # Storing priorities in last chunk [ptr - chunk_size + 1, ptr - 1]
 
     def update(self, start_index: int, priorities: jax.Array) -> "SumTree":
         batch_size = priorities.shape[0]
-        leaf_indices = (start_index + jnp.arange(batch_size)) % self.active_leaves
+        trailing_size = self.chunk_size - 1
+
+        # Include both masked and new priorities
+        start = start_index - trailing_size
+        offset = trailing_size + batch_size
+
+        # To recover the last masked priorities
+        leaf_indices = (start + jnp.arange(offset)) % self.active_leaves
         tree_indices = leaf_indices + self.capacity - 1
 
-        deltas = priorities - self.tree[tree_indices]
-        new_tree = self.tree.at[tree_indices].set(priorities)
+        merged_priorities = jnp.concatenate([self.masked_priorities, priorities])
+        new_masked_priorities = merged_priorities[-trailing_size:] # Keep track of the new masked priorities
+        merged_priorities = merged_priorities.at[-trailing_size:].set(0.) # Mask out the forbidden sampling regime
+        deltas = merged_priorities - self.tree[tree_indices]
+        new_tree = self.tree.at[tree_indices].set(merged_priorities)
 
         curr_index = tree_indices
         # For small depth, for loop is preferred
@@ -123,7 +137,7 @@ class SumTree(eqx.Module):
             curr_index = (curr_index - 1) // 2
             new_tree = new_tree.at[curr_index].add(deltas)
 
-        return eqx.tree_at(lambda t: t.tree, self, new_tree)
+        return eqx.tree_at(lambda t: (t.tree, t.masked_priorities), self, (new_tree, new_masked_priorities))
 
     def sample(self, batch_size: int, key: jax.Array) -> Tuple[jax.Array, jax.Array]:
         total_priority = self.tree[0]
@@ -139,9 +153,7 @@ class SumTree(eqx.Module):
             right = left + 1
 
             left_val = self.tree[left]
-
             go_right = s_val > left_val
-
             next_i = jnp.where(go_right, right, left)
             next_s = jnp.where(go_right, s_val - left_val, s_val)
             return (next_i, next_s)
@@ -158,16 +170,17 @@ class Prioritized(Uniform):
     alpha: float = eqx.field(static=True)
     beta: float = eqx.field(static=True)
 
-    def __init__(self, capacity: int, obs_shape: Tuple[int, ...], action_dim: int, alpha=0.6, beta=0.4):
+    # Require chunk_size to be given because we want to handle the overshooting during settling priorities
+    def __init__(self, capacity: int, obs_shape: Tuple[int, ...], action_dim: int, chunk_size: int, alpha=0.6, beta=0.4):
         super().__init__(capacity, obs_shape, action_dim)
         self.alpha = alpha
         self.beta = beta
-        self.sumtree = SumTree(capacity)
+        self.sumtree = SumTree(capacity, chunk_size)
 
     def add(self, transition: Transition, priority: jax.Array):
         """Adds a single or a batch of transitions to the buffer."""
         # Add priority first to use old parameters
-        new_tree = self.sumtree.update(self.ptr, priority ** self.alpha)
+        new_tree = self.sumtree.update(self.ptr, priority**self.alpha)
         new_memory = super().add(transition) # updated with new data, ptr and size
 
         # Update additional subtree of new priority
@@ -177,5 +190,5 @@ class Prioritized(Uniform):
             new_tree
         )
 
-    def sample_batch_index(self, batch_size: int, key: PRNGKeyArray): # TODO: redefine
-        return self.sumtree.sample(batch_size, key) # TODO: fix the overshooting
+    def sample_batch_index(self, batch_size: int, key: PRNGKeyArray):
+        return self.sumtree.sample(batch_size, key)
