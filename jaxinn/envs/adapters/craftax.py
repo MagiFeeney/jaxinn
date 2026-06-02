c4d94e178d711465c50cea15d9a99055fc0df664
Magi Feeney <matrixfeeney@gmail.com>
2026-06-02 09:41:31 +0800

    env: fix Craftax obs_shape

--- jaxinn/envs/adapters/craftax.py
+++ jaxinn/envs/adapters/craftax.py
@@ -1,3 +1,7 @@
+from typing import Tuple
+
+import jax
+
 from envs.adapters.gymnax import Gymnax
 from craftax.craftax_env import make_craftax_env_from_name
 
@@ -10,3 +14,20 @@ class Craftax(Gymnax):
         env = make_craftax_env_from_name("Craftax-" + env_name, **kwargs)
         env_params = env.default_params
         return cls(env, env_params)
+
+    @property
+    def observation_space(self):
+        space = super().observation_space
+        new_shape = self.observation_size
+        return type(space)(
+            low=space.low,
+            high=space.high,
+            shape=new_shape,
+            dtype=space.dtype
+        )
+
+    @property
+    def observation_size(self) -> Tuple[int, ...]:
+        key = jax.random.PRNGKey(0)
+        obs, _ = jax.eval_shape(self.env.reset, key)
+        return obs.shape
