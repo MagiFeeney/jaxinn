bcb95a70d026a0589bcf3a09b232e57f3aa5f593
Magi Feeney <matrixfeeney@gmail.com>
2026-06-04 17:22:03 +0800

    chore: exclude memory from api

--- jaxinn/agent/__init__.py
+++ jaxinn/agent/__init__.py
@@ -1,6 +1,5 @@
 from .rules import Experience, Learner, register_agent, get_agent_cls
 from .losses import differentiable
-from .memory import Memory, Uniform, Prioritized, Batched
 from .models import LatentState, LatentStateWithParams, get_activation_fn, get_precision_fn, StaticCallable, dx, make_mlp
 
 
@@ -9,9 +8,6 @@ __all__ = [
     "get_agent_cls",
     "Experience",
     "Learner",
-    "Memory",
-    "Uniform",
-    "Prioritized",
     "differentiable",
     "LatentState",
     "LatentStateWithParams",
