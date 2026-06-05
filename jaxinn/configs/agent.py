815d19aa32f764c34d312cacfe6412caa2c01c6e
Magi Feeney <matrixfeeney@gmail.com>
2026-06-05 14:54:59 +0800

    update ppo

--- jaxinn/configs/agent.py
+++ jaxinn/configs/agent.py
@@ -48,14 +48,11 @@ class DreamerV2Agent(DreamerAgent):
 # PPO
 @dataclass
 class PPOOptimization(Base):
-    planning_horizon: int = 15
+    clip_param: float = 0.2
+    use_clipped_critic_loss: bool = True
+    num_mini_batch: int = 8
     discount_factor: float = 0.99
     uae_lambda: float = 0.95
-    batch_size: int = 50
-    chunk_size: int = 50
-    free_nats: float = 3.0
-    kl_average: bool = False
-    kl_balance: float = 0.0
 
 
 @dataclass
