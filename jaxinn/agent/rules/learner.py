7e4493cc5463c6407beb36ea0279164e93bf51d3
Magi Feeney <matrixfeeney@gmail.com>
2026-06-20 16:15:09 +0800

    perf: reduce python overhead for learner

--- jaxinn/agent/rules/learner.py
+++ jaxinn/agent/rules/learner.py
@@ -1,6 +1,7 @@
-from typing import Generic, TypeVar
+from typing import Generic, TypeVar, Any
 
-from jaxtyping import PRNGKeyArray
+import jax
+from jaxtyping import PRNGKeyArray, PyTree, PyTreeDef
 import equinox as eqx
 import optax
 
@@ -8,12 +9,20 @@ ModelType = TypeVar("ModelType", bound=eqx.Module)
 
 
 class Learner(eqx.Module, Generic[ModelType]):
-    model: ModelType
+    dynamic_flatten: PyTree
+    dynamic_treedef: PyTreeDef = eqx.field(static=True)
+    static:  Any = eqx.field(static=True)
     optimizer: optax.GradientTransformation = eqx.field(static=True)
     optimizer_state: optax.OptState
 
+    def __init__(self, model: ModelType, optimizer: optax.GradientTransformation):
+        dynamic, self.static = eqx.partition(model, eqx.is_inexact_array)
+        self.dynamic_flatten, self.dynamic_treedef = jax.tree.flatten(dynamic)
+        self.optimizer = optimizer
+        self.optimizer_state = self.optimizer.init(self.dynamic_flatten)
+
     @classmethod
-    def create(cls, model_cls, config, *, key: PRNGKeyArray) -> "Learner[ModelType]":
+    def create(cls, model_cls, config, *, key: PRNGKeyArray) -> "Learner":
         if hasattr(model_cls, "create"): # nested / multiple routes
             model = model_cls.create(config, key=key)
         else:                            # primitive
@@ -22,22 +31,26 @@ class Learner(eqx.Module, Generic[ModelType]):
             optax.clip_by_global_norm(config.optimizer.max_norm),
             optax.adam(config.optimizer.lr, eps=config.optimizer.eps)
         )
-        params = eqx.filter(model, eqx.is_inexact_array)
-        optimizer_state = optimizer.init(params)
-        return cls(model, optimizer, optimizer_state)
+        dynamic, static = eqx.partition(model, eqx.is_inexact_array)
+        dynamic_flatten, dynamic_treedef = jax.tree.flatten(dynamic)
+        optimizer_state = optimizer.init(dynamic_flatten)
+        return cls(dynamic_flatten, dynamic_treedef, static, optimizer, optimizer_state)
 
     def update(self, grads) -> "Learner":
         if isinstance(grads, Learner):
-            grads = grads.model
-        updates, new_optimizer_state = self.optimizer.update(
-            grads, self.optimizer_state, self.model
-        )
-        new_model = eqx.apply_updates(self.model, updates)
+            grads = grads.dynamic_flatten
+        updates, new_optimizer_state = self.optimizer.update(grads, self.optimizer_state)
+        new_dynamic_flatten = eqx.apply_updates(self.dynamic_flatten, updates)
         return eqx.tree_at(
-            lambda x: (x.model, x.optimizer_state),
+            lambda x: (x.dynamic_flatten, x.optimizer_state),
             self,
-            (new_model, new_optimizer_state)
+            (new_dynamic_flatten, new_optimizer_state)
         )
+
+    @property
+    def model(self):
+        dynamic = jax.tree.unflatten(self.dynamic_treedef, self.dynamic_flatten)
+        return eqx.combine(dynamic, self.static)
 
     def __getattr__(self, name):
         return getattr(self.model, name)
