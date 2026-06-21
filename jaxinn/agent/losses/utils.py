be6d456ce8c2adf03ed7ebe0dd9d04593059e963
Magi Feeney <matrixfeeney@gmail.com>
2026-06-21 17:53:23 +0800

    feat: wrapper for value_and_grad

--- jaxinn/agent/losses/utils.py
+++ jaxinn/agent/losses/utils.py
@@ -2,7 +2,7 @@ import jax
 import equinox as eqx
 import functools
 
-from typing import Union, Sequence
+from typing import Union, Sequence, Optional, Callable
 
 
 def differentiable(fields: Union[str, Sequence[str]]):
@@ -28,3 +28,45 @@ def differentiable(fields: Union[str, Sequence[str]]):
             return func(mixed_agent, *args, **kwargs)
         return body
     return wrapper
+
+
+def value_and_grad(
+        loss_fn: Optional[Callable] = None,
+        *,
+        fields: Optional[Union[str, Sequence[str]]] = None,
+        **grad_kwargs
+):
+
+    """
+    A wrapper around `eqx.filter_value_and_grad` that uses `differentiable`
+    to determine which arguments participate in differentiation.
+
+    This function can be used either directly or as a decorator. When used
+    as a decorator, all arguments must be specified explicitly. In contrast to `differentiable`, it additionally extracts gradients for the selected modules.
+    """
+
+    def wrapper(loss_fn):
+        if fields is not None:
+            _fields = (fields,) if isinstance(fields, str) else tuple(fields)
+            def selector(a):
+                return tuple(getattr(a, f) for f in _fields)
+        else:
+            selector = None
+
+        diff_fn = differentiable(_fields)(loss_fn) if selector is not None else loss_fn
+        grad_fn = eqx.filter_value_and_grad(diff_fn, **grad_kwargs)
+
+        @functools.wraps(loss_fn)
+        def selected_grad_fn(model, *args, **kwargs):
+            out, grads = grad_fn(model, *args, **kwargs)
+            grads = selector(grads)
+            if len(grads) == 1:
+                grads = grads[0]
+            return out, grads
+
+        return selected_grad_fn
+
+    if loss_fn is not None:
+        return wrapper(loss_fn)
+    else:
+        return wrapper
