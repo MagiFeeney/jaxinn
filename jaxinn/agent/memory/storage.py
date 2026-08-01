fix!: cpu memory broadcast bug

jaxinn/agent/memory/storage.py
@@ -38,7 +38,7 @@ class HostRAM:
         token = np.zeros_like(seed_idx, dtype=np.int32) # dummy output
         mask = index != self.length
         valid_index = index[mask]
-        seed_idx = seed_idx.reshape(-1, *(1,) * (valid_index.ndim - 1))
+        seed_idx = np.broadcast_to(seed_idx[..., None], index.shape)[mask]
         jax.tree.map(lambda arr, new: arr.__setitem__((seed_idx, valid_index), new[mask]), self.data, transition)
         return token
 