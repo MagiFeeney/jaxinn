9caa2b7abf0e665c5b7c68fa44f6de139136e69b
Magi Feeney <matrixfeeney@gmail.com>
2026-08-05 21:49:32 +0800

    chore: merge utils

--- jaxinn/agent/rules/utils.py
+++ jaxinn/agent/rules/utils.py
@@ -155,14 +155,17 @@ def staircase_lr_schedule(init_lr, num_iterations, updates_per_iteration):
     return schedule
 
 
-def reconstruct_rl_tuple(transition: Transition, boundary_obs: jax.Array | None = None) -> tuple[jax.Array, jax.Array, jax.Array, jax.Array, jax.Array, jax.Array]:
+def reconstruct_rl_tuple(transition: Transition, boundary_obs: jax.Array | None = None) -> tuple[jax.Array, jax.Array, jax.Array, jax.Array, jax.Array]:
+    terminated_or_boundary = transition.terminated[1:]
+    if boundary_obs is None:    # Next-step autoreset introduces a dummy transition from terminated/truncated obs to the reset obs
+        last_done = transition.terminated[:-1] | transition.truncated[:-1]
+        terminated_or_boundary |= last_done
     return (
         transition.next_obs[:-1],
         transition.action[1:],
         transition.reward[1:],
         transition.next_obs[1:] if boundary_obs is None else boundary_obs[1:],
-        transition.terminated[1:],
-        transition.truncated[1:],
+        terminated_or_boundary
     )
 
 
